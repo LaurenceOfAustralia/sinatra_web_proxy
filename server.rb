805687eec04765ee3026d1398a6ab02938856b1e
Sag0Sag0 <laurencebelastevens@gmail.com>
2017-02-16 17:52:02 +1100

    changed style

--- server.rb
+++ server.rb
@@ -9,3 +9,9 @@ post "/" do
   url = params[:url]
   open(url)
 end
+
+#<form action="/" method="post" class="light">
+#  <h1>Proxy Server</h1>
+#<input name="url" type="text" placeholder="Website address">
+#<input type="submit" value="Submit">
+#</form>
